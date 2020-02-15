8c4217f2de29c72246c4eb2813c4e841a6b349dc
_lucasborges <lucas.borges@dbccompany.com.br>
2020-02-15 19:55:55 -0300

    Obtendo um usuario por id

--- src/main/java/com/devlucas19/workshopmongo/services/UserService.java
+++ src/main/java/com/devlucas19/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.devlucas19.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devlucas19.workshopmongo.domain.UserEntity;
 import com.devlucas19.workshopmongo.repository.UserRepository;
+import com.devlucas19.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<UserEntity> findAll() {
 		return repo.findAll();
+	}
+	
+	public UserEntity findById( String id ) {
+		Optional<UserEntity> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException( "Objeto não encontrado" ));
 	}
 }
